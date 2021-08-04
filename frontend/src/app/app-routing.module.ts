901320c9f8ef47c724c7b53b486f37952f31fe88
Francisco Alex <franciscoalex2407@gmail.com>
2021-08-04 17:34:50 -0300

    ajustes de lib e comps init

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: '', loadChildren: () => import('./pages/admin/admin.module').then(m => m.AdminModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/pages/admin/admin.module.ts
+++ frontend/src/app/pages/admin/admin.module.ts
@@ -1,15 +1,29 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AdminComponent } from './admin.component';
+import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { FormsModule } from '@angular/forms';
+import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
 
 
-
+const routes: Routes = [
+  {
+    path: '', component: AdminComponent, children: [
+      { path: 'dashboard', component: DashboardComponent }
+    ]
+  }
+]
 @NgModule({
   declarations: [
-    AdminComponent
+    AdminComponent,
+    DashboardComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    NgbDropdownModule,
+    RouterModule.forChild(routes)
   ]
 })
 export class AdminModule { }
